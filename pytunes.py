665491f62baa91d3b9026d6a0175fea355ddd57f
Hayes Williams <hwpi31@gmail.com>
2010-08-19 03:30:52 -0700

    Implemented three new methods.

--- pytunes.py
+++ pytunes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import win32com.client as com
+import win32com.client
+import winerror
 
 class BaseClassOnlyError(Exception):
     "This exception is used for classes that are not for actual use, only \
@@ -15,9 +16,29 @@ class __Base(object):
         don't use it except as a template."
 
 class iTunes(__Base):
-    "This is the main liTunes COM object wrapper."
+    "This is the main liTunes COM object wrapper. \
+    3/80 methods complete."
     def __init__(self):
         self.wrapped = win32com.client.Dispatch("iTunes.Application")
         
-
+    def BackTrack(self):
+        "Reposition to the beginning of the current track or go to the previous track if already at start of current track."
+        returned = self.wrapped.BackTrack()
+        if returned != winerror.E_FAIL:
+            raise UnexpectedError
+        return True
         
+    def Play(self):
+        "Play the currently targeted track."
+        returned = self.wrapped.Play()
+        if returned != winerror.E_FAIL:
+            raise UnexpectedError
+        return True
+        
+    def PlayPause(self):
+        returned = self.wrapped.PlayPause()
+        if returned != winerror.E_FAIL:
+            raise UnexpectedError
+        return True
+    
+    
