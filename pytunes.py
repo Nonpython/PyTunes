999d86b7753aea3328bea6cd78508b3999d5c615
Hayes Williams <hwpi31@gmail.com>
2010-08-19 04:27:12 -0700

    Made __Base actually be useful. Added LibraryPlaylist to iTunes. Made a wrapper for IITObject.

--- pytunes.py
+++ pytunes.py
@@ -9,15 +9,50 @@ class BaseClassOnlyError(Exception):
 class UnexpectedError(Exception):
     pass
 
+class ReadOnlyError(Exception):
+    pass
+
+
 class __Base(object):
-    def __init__(self):
-        self.wrapped = None
-        raise BaseClassOnlyError, "This is just a template class, \
-        don't use it except as a template."
+    def __init__(self, wrapped=None):
+        self.wrapped = wrapped
+
+class Object(__Base):
+    "This wraps IITObject."
+    def __getattr__(self, attr):
+        if attr == 'Name':
+            return str(self.wrapped.Name)
+        elif attr == 'Index':
+            return str(self.wrapped.Index)
+        elif attr == 'PlaylistID':
+            return str(self.wrapped.PlaylistID)
+        elif attr == 'SourceID':
+            return str(self.wrapped.SourceID)
+        elif attr == 'TrackDatabaseID':
+            return str(self.wrapped.TrackDatabaseID)
+        elif attr == 'TrackID':
+            return str(self.wrapped.TrackID)
+        else:
+            raise AttributeError, "'%s' does not exist.".format(attr)
+    
+    def __setattr__(self, attr, value):
+        if attr == 'Name':
+            if isinstance(value, basestring):
+                self.wrapped.Name = value
+            else:
+                raise TypeError, "The value of Name must be a 'str' or a \
+                'unicode'."
+        else:
+            raise AttributeError, "'%s' does not exist or it is read only."
+
+class LibraryPlaylist(Object):
+    "This is a wrapper for IITLibraryPlaylist. \
+    0/0"
+
 
 class iTunes(__Base):
     "This is the main liTunes COM object wrapper. \
-    3/80 methods complete."
+    4/80 methods complete."
     def __init__(self):
         self.wrapped = win32com.client.Dispatch("iTunes.Application")
         
@@ -41,4 +76,5 @@ class iTunes(__Base):
             raise UnexpectedError
         return True
     
-    
+    def LibraryPlaylist(self):
+        return LibraryPlaylist(self.wrapped.LibraryPlaylist())
