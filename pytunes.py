aa6021301155254de6c55b3b58d51c905dce99e0
Hayes Williams <hwpi31@gmail.com>
2010-09-09 01:37:41 -0700

    Deleted unneeded support files. Completely redesigned Object.

--- pytunes.py
+++ pytunes.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 import win32com.client
 import winerror
-
-class BaseClassOnlyError(Exception):
-    "This exception is used for classes that are not for actual use, only \
-    as a template for subclassing."
+import support.singleton as singleton
 
 class UnexpectedError(Exception):
     pass
@@ -14,65 +11,70 @@ class ReadOnlyError(Exception):
 
 
 class __Base(object):
-    def __init__(self, wrapped=None):
-        self.wrapped = wrapped
+    pass
 
 class Object(__Base):
     "This wraps IITObject."
-    def __getattr__(self, attr):
-        if attr == 'Name':
-            return str(self.wrapped.Name)
-        elif attr == 'Index':
-            return str(self.wrapped.Index)
-        elif attr == 'PlaylistID':
-            return str(self.wrapped.PlaylistID)
-        elif attr == 'SourceID':
-            return str(self.wrapped.SourceID)
-        elif attr == 'TrackDatabaseID':
-            return str(self.wrapped.TrackDatabaseID)
-        elif attr == 'TrackID':
-            return str(self.wrapped.TrackID)
-        else:
-            raise AttributeError, "'%s' does not exist.".format(attr)
+    ROSlices = {
+        'Index': lambda: str(self.wrapped.Index),
+        'PlaylistID': lambda: str(self.wrapped.PlaylistID),
+        'SourceID': lambda: str(self.wrapped.SourceID),
+        'TrackDatabaseID': lambda: str(self.wrapped.TrackDatabaseID),
+        'TrackID': lambda: str(self.wrapped.TrackID)
+        }
     
-    def __setattr__(self, attr, value):
-        if attr == 'Name':
-            if isinstance(value, basestring):
-                self.wrapped.Name = value
-            else:
-                raise TypeError, "The value of Name must be a 'str' or a \
-                'unicode'."
+    def _nameslice(self, x):
+        if x is not None:
+            self.wrapped.Name = x
+            return self.wrapped.Name
         else:
-            raise AttributeError, "'%s' does not exist or it is read only."
+            return self.wrapped.Name
+    
+    RWSlices = {'Name': lambda x: self._nameslice}
+    
+    def __getitem__(self, key):
+        try:
+            return ROSlices[key]()
+        except KeyError:
+            raise KeyError, "'%s' is not a valid property." % (key)
+    
+    def __setitem__(self, key, value):
+        try:
+            RWSlices[key](value)
+            return ROSlices[key]()
+        except KeyError:
+            raise KeyError, "'%s' is not a valid property or it is read only." % (key)
 
 class LibraryPlaylist(Object):
     "This is a wrapper for IITLibraryPlaylist. \
     0/0"
+    
 
 
-class iTunes(__Base):
+class iTunes(__Base, singleton.Singleton):
     "This is the main liTunes COM object wrapper. \
     4/80 methods complete."
     def __init__(self):
         self.wrapped = win32com.client.Dispatch("iTunes.Application")
         
     def BackTrack(self):
-        "Reposition to the beginning of the current track or go to the previous track if already at start of current track."
+        "Reposition to the beginning of the current track or go to the previous \
+track if already at start of current track."
         returned = self.wrapped.BackTrack()
-        if returned != winerror.E_FAIL:
+        if returned == winerror.E_FAIL:
             raise UnexpectedError
         return True
         
     def Play(self):
         "Play the currently targeted track."
         returned = self.wrapped.Play()
-        if returned != winerror.E_FAIL:
+        if returned == winerror.E_FAIL:
             raise UnexpectedError
         return True
         
     def PlayPause(self):
         returned = self.wrapped.PlayPause()
-        if returned != winerror.E_FAIL:
+        if returned == winerror.E_FAIL:
             raise UnexpectedError
         return True
     
